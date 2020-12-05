c98512219bba673901a99b5604ad38bbe57d3d91
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2020-12-05 12:36:13 +0200

    Initial import.

---
+++ include/tcl.h
@@ -0,0 +1,94 @@
+#define FFI_SCOPE "tcl"
+#define FFI_LIB "libtcl8.6.so"
+
+typedef void (Tcl_FreeProc) (char *blockPtr);
+
+typedef struct Tcl_Interp
+{
+    char *result;
+    Tcl_FreeProc *freeProc;
+    int errorLine;
+} Tcl_Interp;
+
+typedef void (Tcl_FreeInternalRepProc) (struct Tcl_Obj *objPtr);
+typedef void (Tcl_DupInternalRepProc) (struct Tcl_Obj *srcPtr, struct Tcl_Obj *dupPtr);
+typedef void (Tcl_UpdateStringProc) (struct Tcl_Obj *objPtr);
+typedef int (Tcl_SetFromAnyProc) (Tcl_Interp *interp, struct Tcl_Obj *objPtr);
+
+typedef struct Tcl_ObjType {
+    const char *name;
+    Tcl_FreeInternalRepProc *freeIntRepProc;
+    Tcl_DupInternalRepProc *dupIntRepProc;
+    Tcl_UpdateStringProc *updateStringProc;
+    Tcl_SetFromAnyProc *setFromAnyProc;
+} Tcl_ObjType;
+
+typedef struct Tcl_Obj {
+    int refCount;
+    char *bytes;
+    int length;
+    const Tcl_ObjType *typePtr;
+    union {
+        long longValue;
+        double doubleValue;
+        void *otherValuePtr;
+        long long wideValue;
+        struct {
+            void *ptr1;
+            void *ptr2;
+        } twoPtrValue;
+        struct {
+            void *ptr;
+            unsigned long value;
+        } ptrAndLongRep;
+    } internalRep;
+} Tcl_Obj;
+
+Tcl_Interp* Tcl_CreateInterp(void);
+int Tcl_Init(Tcl_Interp *interp);
+
+int Tcl_GetErrorLine(Tcl_Interp *interp);
+
+int Tcl_Eval(Tcl_Interp *interp, const char *script);
+int Tcl_EvalObj(Tcl_Interp *interp, Tcl_Obj *objPtr);
+
+Tcl_Obj *Tcl_GetObjResult(Tcl_Interp *interp);
+const char *Tcl_GetString(Tcl_Obj *objPtr);
+Tcl_Obj *Tcl_DuplicateObj(Tcl_Obj *objPtr);
+void TclFreeObj(Tcl_Obj *objPtr);
+int Tcl_GetBoolean(Tcl_Interp *interp, const char *src, int *boolPtr);
+int Tcl_GetBooleanFromObj(Tcl_Interp *interp, Tcl_Obj *objPtr, int *boolPtr);
+unsigned char *Tcl_GetByteArrayFromObj(Tcl_Obj *objPtr, int *lengthPtr);
+int Tcl_GetDouble(Tcl_Interp *interp, const char *src, double *doublePtr);
+int Tcl_GetDoubleFromObj(Tcl_Interp *interp, Tcl_Obj *objPtr, double *doublePtr);
+int Tcl_GetIndexFromObj(Tcl_Interp *interp, Tcl_Obj *objPtr,
+	    const char *const *tablePtr, const char *msg, int flags, int *indexPtr);
+int Tcl_GetInt(Tcl_Interp *interp, const char *src, int *intPtr);
+int Tcl_GetIntFromObj(Tcl_Interp *interp, Tcl_Obj *objPtr, int *intPtr);
+int Tcl_GetLongFromObj(Tcl_Interp *interp, Tcl_Obj *objPtr, long *longPtr);
+const Tcl_ObjType *Tcl_GetObjType(const char *typeName);
+const char *Tcl_GetStringFromObj(Tcl_Obj *objPtr, int *lengthPtr);
+void Tcl_InvalidateStringRep(Tcl_Obj *objPtr);
+int Tcl_ListObjAppendList(Tcl_Interp *interp, Tcl_Obj *listPtr, Tcl_Obj *elemListPtr);
+int Tcl_ListObjAppendElement(Tcl_Interp *interp, Tcl_Obj *listPtr, Tcl_Obj *objPtr);
+int Tcl_ListObjGetElements(Tcl_Interp *interp, Tcl_Obj *listPtr,
+	    int *objcPtr, Tcl_Obj ***objvPtr);
+int Tcl_ListObjIndex(Tcl_Interp *interp, Tcl_Obj *listPtr, int index,
+	    Tcl_Obj **objPtrPtr);
+int Tcl_ListObjLength(Tcl_Interp *interp, Tcl_Obj *listPtr,
+	    int *lengthPtr);
+int Tcl_ListObjReplace(Tcl_Interp *interp, Tcl_Obj *listPtr, int first,
+	    int count, int objc, Tcl_Obj *const objv[]);
+Tcl_Obj *Tcl_NewBooleanObj(int boolValue);
+Tcl_Obj *Tcl_NewByteArrayObj(const unsigned char *bytes, int length);
+Tcl_Obj *Tcl_NewDoubleObj(double doubleValue);
+
+const char *Tcl_GetVar(Tcl_Interp *interp, const char *varName, int flags);
+const char *Tcl_GetVar2(Tcl_Interp *interp, const char *part1, const char *part2, int flags);
+const char *Tcl_SetVar(Tcl_Interp *interp, const char *varName,
+                        const char *newValue, int flags);
+const char *Tcl_SetVar2(Tcl_Interp *interp, const char *part1, 
+                        const char *part2, const char *newValue, int flags);
+
+void TclFreeObj(Tcl_Obj *objPtr);
+void Tcl_FreeResult(Tcl_Interp *interp);
