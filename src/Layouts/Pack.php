6dae662360c242a5f3af1b175e9d22ddae063c3d
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2020-12-12 21:12:18 +0200

    pack geometry manager.

---
+++ src/Layouts/Pack.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Layouts;
+
+use TclTk\Options;
+use TclTk\Widgets\TkWidget;
+
+class Pack
+{
+    private TkWidget $widget;
+
+    public function __construct(TkWidget $widget)
+    {
+        $this->widget = $widget;
+    }
+
+    public function pack(array $options = [])
+    {
+        $this->widget->exec('pack', $this->widget->path(), new Options($options));
+    }
+}
