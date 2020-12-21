6d227223f95d43e7ad0e012e0284618baad376af
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2020-12-21 22:28:15 +0200

    Entry widget.

---
+++ src/Widgets/Entry.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Widgets;
+
+use TclTk\Options;
+
+/**
+ * Implementation of Tk entry widget.
+ *
+ * @link https://www.tcl.tk/man/tcl8.6/TkCmd/entry.htm
+ */
+class Entry extends Widget
+{
+    public function __construct(TkWidget $parent, array $options = [])
+    {
+        parent::__construct($parent, 'entry', 'e', $options);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function initOptions(): Options
+    {
+        return parent::initOptions()->mergeAsArray([
+            'disabledBackground' => null,
+            'disabledForeground' => null,
+            'invalidCommand' => null,
+            'readonlyBackground' => null,
+            'show' => null,
+            'state' => null,
+            'validate' => null,
+            'validateCommand' => null,
+            'width' => null,
+        ]);
+    }
+
+    /**
+     * Returns the entry's string.
+     *
+     * @link https://www.tcl.tk/man/tcl8.6/TkCmd/entry.htm#M45
+     */
+    public function value(): string
+    {
+        return $this->window()->exec('get');
+    }
+
+    /**
+     * Delete one or more elements of the entry.
+     *
+     * @link https://www.tcl.tk/man/tcl8.6/TkCmd/entry.htm#M44
+     */
+    public function delete(int $first, int $last = 0): self
+    {
+        $params = [$first];
+        if ($last > 0) {
+            $params[] = $last;
+        }
+        $this->window()->exec('delete', $params);
+        return $this;
+    }
+
+    /**
+     * Insert a string just before the specified index.
+     *
+     * @link https://www.tcl.tk/man/tcl8.6/TkCmd/entry.htm#M48
+     */
+    public function insert(int $index, string $str): self
+    {
+        $this->window()->exec('insert', [$index, $str]);
+        return $this;
+    }
+}
