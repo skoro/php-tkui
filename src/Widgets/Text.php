ac582530ef0d58a56972621086c244cec35b52ac
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2020-12-22 22:25:51 +0200

    Initial text widget.

---
+++ src/Widgets/Text.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Widgets;
+
+use TclTk\Options;
+
+/**
+ * Implementation of Tk text widget.
+ *
+ * @link https://www.tcl.tk/man/tcl8.6/TkCmd/text.htm
+ */
+class Text extends Widget
+{
+    /**
+     * States for the 'state' option.
+     */
+    const STATE_NORMAL = 'normal';
+    const STATE_DISABLED = 'disabled';
+
+    public function __construct(TkWidget $parent, array $options = [])
+    {
+        parent::__construct($parent, 'text', 't', $options);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function initWidgetOptions(): Options
+    {
+        return new Options([
+            'autoSeparators' => null,
+            'blockCursor' => null,
+            'endLine' => null,
+            'height' => null,
+            'inactiveSelectBackground' => null,
+            'insertUnfocussed' => null,
+            'maxUndo' => null,
+            'spacing1' => null,
+            'spacing2' => null,
+            'spacing3' => null,
+            'startLine' => null,
+            'state' => null,
+            'tabs' => null,
+            'tabStyle' => null,
+            'undo' => null,
+            'width' => null,
+            'wrap' => null,
+        ]);
+    }
+}
