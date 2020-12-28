fee54b475aca1bd6c21220a1f6c366830f8a5ac2
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2020-12-28 20:21:09 +0200

    Scrollable widget.

---
+++ src/Widgets/ScrollableWidget.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Widgets;
+
+use InvalidArgumentException;
+
+abstract class ScrollableWidget extends Widget
+{
+    private Scrollbar $xScroll;
+    private Scrollbar $yScroll;
+
+    /**
+     * @inheritdoc
+     */
+    public function __get($name)
+    {
+        switch ($name) {
+            case 'xScrollCommand':
+                return $this->xScroll;
+            case 'yScrollCommand':
+                return $this->yScroll;
+        }
+        return parent::__get($name);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __set($name, $value)
+    {
+        switch ($name) {
+            case 'xScrollCommand':
+            case 'yScrollCommand':
+                if (!($value instanceof Scrollbar)) {
+                    throw new InvalidArgumentException("$name must be an instance of " . Scrollbar::class);
+                }
+                switch ($name) {
+                    case 'xScrollCommand':
+                        $this->xScroll = $value;
+                        break;
+                    case 'yScrollCommand':
+                        $this->yScroll = $value;
+                        break;
+                }
+                parent::__set($name, $value->path() . ' set');
+                $value->command = $this;
+                break;
+
+            default:
+                parent::__set($name, $value);
+        }
+    }
+}
