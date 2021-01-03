f5c25f4de3ca96e89d8e7450a88c16b4241c90f0
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-03 17:20:42 +0200

    Window tests.

--- tests/Widgets/WindowTest.php
+++ tests/Widgets/WindowTest.php
@@ -3,8 +3,31 @@
 namespace TclTk\Tests\Widgets;
 
 use TclTk\Tests\TestCase;
+use TclTk\Widgets\Window;
 
 class WindowTest extends TestCase
 {
-    
+    /** @test */
+    public function root_window()
+    {
+        $this->tclEvalTest(1, [
+            ['wm', 'title', '.', '{Test}']
+        ]);
+        new Window($this->app, 'Test');
+    }
+
+    /** @test */
+    public function second_window()
+    {
+        // Because of static window's id all subsequent windows
+        // will be created throughout of toplevel.
+        $this->tclEvalTest(4, [
+            ['toplevel', $this->checkWidget('.w')],
+            ['wm', 'title', $this->checkWidget('.w'), '{Win 1}'],
+            ['toplevel', $this->checkWidget('.w')],
+            ['wm', 'title', $this->checkWidget('.w'), '{Win 2}'],
+        ]);
+        new Window($this->app, 'Win 1');
+        new Window($this->app, 'Win 2');
+    }
 }
