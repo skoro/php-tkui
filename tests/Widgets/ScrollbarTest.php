cdf8915d0e0572ded4a4608732bda79afb54519d
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-03 11:50:35 +0200

    Scrollbar tests.

--- tests/Widgets/ScrollbarTest.php
+++ tests/Widgets/ScrollbarTest.php
@@ -3,8 +3,64 @@
 namespace TclTk\Tests\Widgets;
 
 use TclTk\Tests\TestCase;
+use TclTk\Widgets\ScrollableWidget;
+use TclTk\Widgets\Scrollbar;
 
 class ScrollbarTest extends TestCase
 {
-    
+    /** @test */
+    public function vertical_orient_by_default()
+    {
+        $this->tclEvalTest(1, [
+            ['scrollbar', $this->checkWidget('.sb'), '-orient', 'vertical'],
+        ]);
+        new Scrollbar($this->createWindowStub());
+    }
+
+    /** @test */
+    public function create_horizontal_orient()
+    {
+        $this->tclEvalTest(1, [
+            ['scrollbar', $this->checkWidget('.sb'), '-orient', 'horizontal'],
+        ]);
+        new Scrollbar($this->createWindowStub(), FALSE);
+    }
+
+    /** @test */
+    public function vert_orient_to_view()
+    {
+        $scr = new Scrollbar($this->createWindowStub(), TRUE);
+        $this->assertEquals('yview', $scr->getOrientToView());
+    }
+
+    /** @test */
+    public function horiz_orient_to_view()
+    {
+        $scr = new Scrollbar($this->createWindowStub(), FALSE);
+        $this->assertEquals('xview', $scr->getOrientToView());
+    }
+
+    /** @test */
+    public function assign_scrollable_as_vertical()
+    {
+        $test = $this->createStub(ScrollableWidget::class);
+        $test->method('path')->willReturn('.p');
+
+        $scr = new Scrollbar($this->createWindowStub());
+        $scr->command = $test;
+
+        $this->assertEquals('.p yview', $scr->command);
+    }
+
+    /** @test */
+    public function assign_scrollable_as_horizontal()
+    {
+        $test = $this->createStub(ScrollableWidget::class);
+        $test->method('path')->willReturn('.p');
+
+        $scr = new Scrollbar($this->createWindowStub(), FALSE);
+        $scr->command = $test;
+
+        $this->assertEquals('.p xview', $scr->command);
+    }
 }
