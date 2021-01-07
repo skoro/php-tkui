7899d16926a0fbd189d851f4024a58cad8a2255f
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-07 11:40:11 +0200

    Variable class tests.

---
+++ tests/Tcl/VariableTest.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Tests\Tcl;
+
+use TclTk\Exceptions\TclInterpException;
+use TclTk\Tests\TclInterp;
+use TclTk\Tests\TestCase;
+
+class VariableTest extends TestCase
+{
+    use TclInterp;
+
+    /** @test */
+    public function create_empty_var()
+    {
+        $var = $this->interp->createVariable('myVar');
+        $this->assertEquals('', $var->asString());
+    }
+
+    /** @test */
+    public function create_with_string_value()
+    {
+        $var = $this->interp->createVariable('myVar', NULL, 'abc');
+        $this->assertEquals('abc', $var->asString());
+    }
+
+    /** @test */
+    public function delete_var()
+    {
+        $var = $this->interp->createVariable('myVar', NULL, 'abc');
+        $this->assertEquals('abc', $var->asString());
+        
+        unset($var);
+
+        $this->expectException(TclInterpException::class);
+        $this->expectExceptionMessage('Eval: can\'t read "myVar": no such variable');
+
+        $this->interp->eval('set myVar');
+    }
+
+    /** @test */
+    public function change_var()
+    {
+        $var = $this->interp->createVariable('myVar');
+        $this->assertEquals('', $var->asString());
+        $var->set('test');
+        $this->assertEquals('test', $var->asString());
+        $var->set(99);
+        $this->assertEquals(99, $var->asInt());
+        $var->set(TRUE);
+        $this->assertEquals(TRUE, $var->asBool());
+    }
+
+    /** @test */
+    public function convert_value_from_type_to_type()
+    {
+        $var = $this->interp->createVariable('myVar', NULL, '50');
+        $this->assertEquals('50', $var->asString());
+        $this->assertEquals(50, $var->asInt());
+        $this->assertEquals(50.0, $var->asFloat());
+        $this->assertEquals(TRUE, $var->asBool());
+    }
+
+    /** @test */
+    public function default_value_cannot_be_got_as_int()
+    {
+        $var = $this->interp->createVariable('myVar');
+
+        $this->expectException(TclInterpException::class);
+        $this->expectExceptionMessage('GetLongFromObj: expected integer but got ""');
+
+        $var->asInt();
+    }
+
+    /** @test */
+    public function default_value_cannot_be_got_as_bool()
+    {
+        $var = $this->interp->createVariable('myVar');
+
+        $this->expectException(TclInterpException::class);
+        $this->expectExceptionMessage('GetBooleanFromObj: expected boolean value but got ""');
+
+        $var->asBool();
+    }
+}
