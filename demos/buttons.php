0c52f59d7bf603c5123014ae981d8c4969301393
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-09 15:35:45 +0200

    button callbacks.

--- demos/buttons.php
+++ demos/buttons.php
@@ -3,6 +3,7 @@
 use TclTk\App;
 use TclTk\Widgets\Buttons\Button;
 use TclTk\Widgets\Buttons\CheckButton;
+use TclTk\Widgets\Buttons\RadioButton;
 use TclTk\Widgets\Label;
 use TclTk\Widgets\LabelFrame;
 use TclTk\Widgets\RadioGroup;
@@ -15,8 +16,6 @@ $demo = new class extends Window
     public function __construct()
     {
         parent::__construct(App::create(), 'Buttons Demo');
-        // $this->app()->tk()->interp()->createVariable('w0_wgd', '', 'one');
-        // $this->app()->tk()->interp()->eval('set w0_wgd');
         $this->buttons()->pack()->sideLeft()->ipadX(4)->ipadY(4)->manage();
         $this->checkboxes()->pack()->sideLeft()->manage();
         $this->radiobuttons()->pack()->sideLeft()->manage();
@@ -28,12 +27,12 @@ $demo = new class extends Window
         $l = new Label($f, 'Press button');
         $l->pack()->sideTop()->ipadY(2)->manage();
         (new Button($f, 'Button 1'))
-            ->onClick(fn () => $l->text = 'Button 1')
+            ->onClick(fn (Button $b) => $l->text = $b->text)
             ->pack()
             ->sideTop()
             ->manage();
         (new Button($f, 'Button 2'))
-            ->onClick(fn () => $l->text = 'Button 2')
+            ->onClick(fn (Button $b) => $l->text = $b->text)
             ->pack()
             ->sideTop()
             ->manage();
@@ -43,12 +42,15 @@ $demo = new class extends Window
     protected function checkboxes(): LabelFrame
     {
         $f = new LabelFrame($this, 'Checkboxes');
+        $l = new Label($f, 'Selected...');
+        $l->pack()->manage();
         foreach (['One', 'Two', 'Three', 'Four'] as $name) {
             $cb = new CheckButton($f, $name);
             $cb->pack()->sideTop()->anchor('w')->manage();
             if ($name === 'Three') {
                 $cb->select();
             }
+            $cb->onClick(fn (CheckButton $cb) => $l->text = $cb->text . ': ' . $cb->get());
         }
         return $f;
     }
@@ -56,10 +58,13 @@ $demo = new class extends Window
     protected function radiobuttons(): LabelFrame
     {
         $f = new LabelFrame($this, 'Radio buttons');
+        $l = new Label($f, 'Selected...');
+        $l->pack()->manage();
         $rg = new RadioGroup($f);
         $rg->setValue('two');
         foreach (['One', 'Two', 'Three', 'Four'] as $name) {
             $rg->add($name, strtolower($name))
+                ->onClick(fn (RadioButton $b) => $l->text = $b->text . ': ' . $b->get())
                 ->pack()
                 ->anchor('w')
                 ->manage();
