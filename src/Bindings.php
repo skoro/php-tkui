fa4cbd5cf8a8d1926435181f298f793db364f302
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-18 21:17:00 +0200

    Cleanup callbacks when object is destroyed.

--- src/Bindings.php
+++ src/Bindings.php
@@ -16,13 +16,22 @@ class Bindings
 
     public function __destruct()
     {
-        // TODO: delete all the bindings.
+        foreach ($this->callbacks as $tag => $callbacks) {
+            foreach ($callbacks as $event => $_) {
+                $this->deleteCallback($tag, $event);
+            }
+        }
     }
 
     public function bindWidget(TkWidget $widget, string $event, callable $callback)
     {
         $tag = $widget->path();
         $this->tkBind($tag, $event, $callback);
+    }
+
+    public function unbindWidget(TkWidget $widget, string $event)
+    {
+        $this->deleteCallback($widget->path(), $event);
     }
 
     protected function tkBind(string $tag, string $event, callable $callback)
@@ -35,10 +44,24 @@ class Bindings
         $this->interp->eval($script);
     }
 
+    protected function deleteCallback(string $tag, string $event)
+    {
+        if (isset($this->callbacks[$tag][$event])) {
+            unset($this->callbacks[$tag][$event]);
+            $command = $this->tclCommandName($tag, $event);
+            $this->interp->deleteCommand($command);
+        }
+    }
+
     protected function createTclBindCallback(string $tag, string $event, callable $callback): string
     {
-        $command = 'PHP_Bind_' . str_replace('.', '_', $tag) . '_' . $event;
+        $command = $this->tclCommandName($tag, $event);
         $this->interp->createCommand($command, $callback);
         return $command;
+    }
+
+    protected function tclCommandName(string $tag, string $event): string
+    {
+        return 'PHP_Bind_' . str_replace('.', '_', $tag) . '_' . $event;
     }
 }
