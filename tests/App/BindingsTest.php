fa8a7656537ae125e864e5d19782dd69c514ec55
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-19 22:05:12 +0200

    Binding tests.

---
+++ tests/App/BindingsTest.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Tests\App;
+
+use TclTk\Bindings;
+use TclTk\Interp;
+use TclTk\Tests\TestCase;
+use TclTk\Widgets\Buttons\Button;
+
+class BindingsTest extends TestCase
+{
+    /** @test */
+    public function create_bind_proc()
+    {
+        $interp = $this->createMock(Interp::class);
+        $interp->expects($this->once())
+               ->method('eval')
+               ->with('bind .b1 <Control-a> PHP_Bind__b1_Control-a');
+
+        $bindings = new Bindings($interp);
+
+        $btn = new Button($this->createWindowStub(), 'test');
+        $bindings->bindWidget($btn, 'Control-a', function () {});
+    }
+
+    /** @test */
+    public function only_once_bind_for_same_event()
+    {
+        $interp = $this->createMock(Interp::class);
+        $interp->expects($this->once())
+               ->method('eval')
+               ->with('bind .b2 <Control-a> PHP_Bind__b2_Control-a');
+
+        $bindings = new Bindings($interp);
+
+        $btn = new Button($this->createWindowStub(), 'test');
+        $bindings->bindWidget($btn, 'Control-a', function () {});
+        $bindings->bindWidget($btn, 'Control-a', function () {});
+    }
+
+    /** @test */
+    public function unbind_event()
+    {
+        $interp = $this->createMock(Interp::class);
+        $interp->expects($this->once())
+               ->method('eval')
+               ->with('bind .b3 <Control-a> PHP_Bind__b3_Control-a');
+        $interp->expects($this->once())
+               ->method('deleteCommand')
+               ->with('PHP_Bind__b3_Control-a');
+
+        $bindings = new Bindings($interp);
+
+        $btn = new Button($this->createWindowStub(), 'test');
+        $bindings->bindWidget($btn, 'Control-a', function () {});
+        $bindings->unbindWidget($btn, 'Control-a');
+    }
+}
