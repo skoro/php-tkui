d35fd135814384dee9d22b3936a196df623b06a3
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-26 21:17:44 +0200

    ttk style command

---
+++ src/Style.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace TclTk;
+
+/**
+ * Ttk style.
+ *
+ * @link https://www.tcl.tk/man/tcl8.6/TkCmd/ttk_style.htm
+ */
+class Style
+{
+    private Interp $interp;
+
+    public function __construct(Interp $interp)
+    {
+        $this->interp = $interp;
+    }
+
+    /**
+     * Returns a list of names of installed ttk themes.
+     *
+     * @return string[]
+     */
+    public function themes(): array
+    {
+        $this->call('theme', 'names');
+        return $this->interp->getListResult();
+    }
+
+    /**
+     * @param string $name Ttk theme name.
+     */
+    public function useTheme(string $name): self
+    {
+        $this->call('theme', 'use', Tcl::quoteString($name));
+        return $this;
+    }
+
+    protected function call(...$args)
+    {
+        $this->interp->eval('ttk::style ' . implode(' ', $args));
+    }
+}
