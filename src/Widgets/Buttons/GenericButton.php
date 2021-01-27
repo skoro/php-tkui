f0c44b864c7ddda13c1b8bde81c4e5dbf3ce90aa
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-27 22:05:35 +0200

    Command trait is an implementation of -command option.

--- src/Widgets/Buttons/GenericButton.php
+++ src/Widgets/Buttons/GenericButton.php
@@ -2,7 +2,6 @@
 
 namespace TclTk\Widgets\Buttons;
 
-use InvalidArgumentException;
 use TclTk\Options;
 use TclTk\Widgets\TkWidget;
 use TclTk\Widgets\Widget;
@@ -18,6 +17,8 @@ use TclTk\Widgets\Widget;
  */
 abstract class GenericButton extends TkWidget
 {
+    use Command;
+
     /**
      * States for the 'state' option.
      */
@@ -62,47 +63,6 @@ abstract class GenericButton extends TkWidget
             'command' => null,
             'overRelief' => null,
         ]);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function __set(string $name, $value)
-    {
-        // A special case for 'command' option.
-        // Register a valid callback (Tcl proc) to handle
-        // a button click from php.
-        // Keep in mind that 'command' getter will return
-        // a Tcl script instead of the callback.
-        if ($name === 'command') {
-            if (is_callable($value)) {
-                $this->commandValue = $value;
-                $value = $this->window()->registerCallback($this, $value);
-            } else {
-                throw new InvalidArgumentException(sprintf('"%s" is not a valid button command.', $value));
-            }
-        }
-        parent::__set($name, $value);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function __get($name)
-    {
-        if ($name === 'command') {
-            return $this->commandValue;
-        }
-        return parent::__get($name);
-    }
-
-    /**
-     * Wrapper for 'command' property.
-     */
-    public function onClick(callable $callback): self
-    {
-        $this->command = $callback;
-        return $this;
     }
 
     /**
