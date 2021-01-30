d70b8da9de1b1156e45db95387ce44ac6f86acdd
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-01-30 11:38:02 +0200

    anchor, justify, relief constants.

---
+++ src/Widgets/Consts/Anchor.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Widgets\Consts;
+
+/**
+ * Values for 'anchor' property.
+ */
+interface Anchor
+{
+    const ANCHOR_N = 'n';
+    const ANCHOR_NE = 'ne';
+    const ANCHOR_E = 'e';
+    const ANCHOR_SE = 'se';
+    const ANCHOR_S = 's';
+    const ANCHOR_SW = 'sw';
+    const ANCHOR_W = 'w';
+    const ANCHOR_NW = 'nw';
+    const ANCHOR_CENTER = 'center';
+}
---
+++ src/Widgets/Consts/Relief.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace TclTk\Widgets\Consts;
+
+/**
+ * Values for 'relief' property.
+ */
+interface Relief
+{
+    const RELIEF_FLAT = 'flat';
+    const RELIEF_GROOVE = 'groove';
+    const RELIEF_RAISED = 'raised';
+    const RELIEF_RIDGE = 'ridge';
+    const RELIEF_SOLID = 'solid';
+    const RELIEF_SUNKEN = 'sunken';
+}
