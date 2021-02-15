969ee211da796e9e57584983c14a59dfb6f4992b
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-02-15 21:28:56 +0200

    labels inside labelframe

--- demos/panedwindow.php
+++ demos/panedwindow.php
@@ -2,6 +2,7 @@
 
 use TclTk\App;
 use TclTk\Widgets\Label;
+use TclTk\Widgets\LabelFrame;
 use TclTk\Widgets\PanedWindow;
 use TclTk\Widgets\Window;
 
@@ -16,9 +17,16 @@ $demo = new class extends Window
         $pw = new PanedWindow($this, ['orient' => PanedWindow::ORIENT_HORIZONTAL]);
         $pw->pack()->expand()->fillBoth()->pad(2, 2)->manage();
 
-        $pw->add(new Label($pw, "This is the left side."))
-           ->add(new Label($pw, "This is the middle."))
-           ->add(new Label($pw, 'This is the right side.'));
+        $pw->add($this->makePanel($pw, "Frame 1", "This is the left side."))
+           ->add($this->makePanel($pw, "Frame 2", "This is the middle."))
+           ->add($this->makePanel($pw, "Frame 3", "This is the right side."));
+    }
+
+    protected function makePanel(PanedWindow $parent, string $title, string $label)
+    {
+        $f = new LabelFrame($parent, $title);
+        (new Label($f, $label))->pack()->manage();
+        return $f;
     }
 };
 
