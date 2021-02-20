4e2f7fb363edc088f71d8bedcb4c5f61709f149f
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-02-20 11:31:53 +0200

    Horizontal panedwindow demo.

--- demos/panedwindow.php
+++ demos/panedwindow.php
@@ -14,12 +14,18 @@ $demo = new class extends Window
     {
         parent::__construct(App::create(), 'Paned window demo');
 
-        $pw = new PanedWindow($this, ['orient' => PanedWindow::ORIENT_HORIZONTAL]);
+        $vert = new PanedWindow($this);
+        $vert->pack()->expand()->fillBoth()->pad(2, 2)->manage();
+
+        $pw = new PanedWindow($vert, ['orient' => PanedWindow::ORIENT_HORIZONTAL]);
         $pw->pack()->expand()->fillBoth()->pad(2, 2)->manage();
 
         $pw->add($this->makePanel($pw, "Frame 1", "This is the left side."))
            ->add($this->makePanel($pw, "Frame 2", "This is the middle."))
            ->add($this->makePanel($pw, "Frame 3", "This is the right side."));
+
+        $vert->add($pw);
+        $vert->add($this->makePanel($vert, 'Frame 4', 'Like A Place In The Sun'));
     }
 
     protected function makePanel(PanedWindow $parent, string $title, string $label)
