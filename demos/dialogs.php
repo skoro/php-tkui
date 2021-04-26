90fc63e812250f6b4df496c4d56dd3766aa330db
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-04-26 21:08:46 +0300

    open and save dialogs demo.

---
+++ demos/dialogs.php
@@ -0,0 +1,74 @@
+<?php
+
+use TclTk\App;
+use TclTk\Dialogs\OpenFile;
+use TclTk\Dialogs\SaveFile;
+use TclTk\Widgets\Buttons\Button;
+use TclTk\Widgets\Label;
+use TclTk\Widgets\LabelFrame;
+use TclTk\Widgets\Window;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$demo = new class extends Window
+{
+    public function __construct()
+    {
+        parent::__construct(App::create(), 'Dialogs demo');
+        $this->createOpenDialogFrame();
+        $this->createSaveDialogFrame();
+    }
+
+    private function createOpenDialogFrame()
+    {
+        $f = new LabelFrame($this, 'Open file');
+
+        $b1 = new Button($f, 'Open ...');
+        $b1->pack()->sideLeft()->pad(2, 2)->manage();
+
+        $b2 = new Button($f, 'Open with filter...');
+        $b2->pack()->sideLeft()->pad(2, 2)->manage();
+
+        $res = new Label($f, '');
+        $res->pack()->sideRight()->fillX()->expand()->manage();
+
+        $dlg1 = new OpenFile($this, ['title' => 'Choose a file']);
+        $dlg1->onSuccess(fn ($file) => $res->text = $file);
+        $dlg1->onCancel(fn () => $res->text = 'Cancelled');
+
+        $dlg2 = new OpenFile($this, ['title' => 'Choose a file']);
+        $dlg2->addFileType('PHP', '.php');
+        $dlg2->addFileType('Text files', '.txt');
+        $dlg2->addFileType('All files', '*');
+
+        $dlg2->onSuccess(fn ($file) => $res->text = $file);
+        $dlg2->onCancel(fn () => $res->text = 'Cancelled');
+
+        $b1->onClick([$dlg1, 'showModal']);
+        $b2->onClick([$dlg2, 'showModal']);
+
+        $f->pack()->fillX()->pad(4, 2)->manage();
+    }
+
+    private function createSaveDialogFrame()
+    {
+        $f = new LabelFrame($this, 'Save file');
+
+        $b = new Button($f, 'Save ...');
+        $b->pack()->sideLeft()->pad(2, 2)->manage();
+
+        $res = new Label($f, '');
+        $res->pack()->sideRight()->fillX()->expand()->manage();
+
+        $dlg = new SaveFile($this, ['title' => 'Save the file']);
+        $dlg->initialFile = 'test.txt';
+        $dlg->onSuccess(fn ($file) => $res->text = $file);
+        $dlg->onCancel(fn () => $res->text = 'Cancelled');
+
+        $b->onClick([$dlg, 'showModal']);
+
+        $f->pack()->fillX()->pad(4, 2)->manage();
+    }
+};
+
+$demo->app()->mainLoop();
