3a257bb866926293c910434612defc90d12a610b
Alexei Skorobogatko <skoro@users.noreply.github.com>
2021-05-02 21:10:06 +0300

    Comments.

--- src/Evaluator.php
+++ src/Evaluator.php
@@ -5,31 +5,37 @@ namespace TclTk;
 use TclTk\Widgets\Widget;
 
 /**
- * Provides the access to low-level API.
+ * Provides the access to low-level Tcl API.
  */
 interface Evaluator
 {
     /**
-     * Evaluates a script in the current interepreter.
+     * Evaluates a script in the current interpreter.
      *
-     * @return mixed
+     * @param string ...$args The tokenized Tcl script.
+     *
+     * @return mixed The return value depends on script result.
      */
-    public function tclEval(...$args);
+    public function tclEval(string ...$args);
 
     /**
-     * Registers the variable in the interpreter.
+     * Registers the variable in the current Tcl interpreter.
      *
      * @param Widget|string $varName
      */
     public function registerVar($varName): Variable;
 
     /**
+     * Unregisters the variable.
+     *
      * @param Widget|string $varName
      */
     public function unregisterVar($varName): void;
 
     /**
      * Registers a widget callback.
+     *
+     * @return string Returns a Tcl procedure name.
      */
     public function registerCallback(Widget $widget, callable $callback): string;
 }
