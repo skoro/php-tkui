870d4202ef12cf86e750001e4e6aa8ed81c2a281
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-07-03 17:36:03 +0300

    menu separator item.

---
+++ src/Widgets/Menu/MenuSeparatorItem.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace PhpGui\Widgets\Menu;
+
+/**
+ * Separator menu item.
+ *
+ * A horizontal line which separates menu items from each other.
+ */
+class MenuSeparatorItem extends CommonItem
+{
+    public function type(): string
+    {
+        return 'separator';
+    }
+}
