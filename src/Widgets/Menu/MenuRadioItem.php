0b388f0a9b65b60f28ab8c05485c30c16a49b355
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-07-06 22:54:45 +0300

    menu radio item variable.

--- src/Widgets/Menu/MenuRadioItem.php
+++ src/Widgets/Menu/MenuRadioItem.php
@@ -2,9 +2,12 @@
 
 namespace PhpGui\Widgets\Menu;
 
+use PhpGui\Exceptions\UninitializedVariableException;
 use PhpGui\Options;
 use PhpGui\TclTk\Variable;
 use PhpGui\Widgets\Common\ValueInVariable;
+use PhpGui\Widgets\TkWidget;
+use SplObserver;
 
 /**
  * Implementation of menu radio button item.
@@ -15,17 +18,16 @@ use PhpGui\Widgets\Common\ValueInVariable;
  */
 class MenuRadioItem extends MenuItem implements ValueInVariable
 {
-    /** @var mixed */
-    private $value;
+    private string $value;
 
     /**
-     * @param mixed $value
      * @param callable|null $callback
      */
-    public function __construct(string $label, $value, $callback = null)
+    public function __construct(string $label, string $value, $callback = null, array $options = [])
     {
         parent::__construct($label, $callback);
-        $this->value = $value;
+        $this->variable = $options['variable'] ?? null;
+        $this->setValue($value);
     }
 
     public function type(): string
@@ -46,19 +48,45 @@ class MenuRadioItem extends MenuItem implements ValueInVariable
     }
 
     /**
-     * @param mixed $value
+     * @param string $value
      */
     public function setValue($value): self
     {
         $this->value = $value;
+        if ($this->variable) {
+            $this->variable->set($value);
+            $this->notify();
+        }
         return $this;
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getValue()
     {
-        return $this->value;
+        $this->ensureVariable();
+        return $this->variable->asString();
+    }
+
+    protected function ensureVariable(): void
+    {
+        if (! $this->variable) {
+            throw new UninitializedVariableException();
+        }
+    }
+
+    public function attach(SplObserver $observer)
+    {
+        parent::attach($observer);
+        if ($observer instanceof TkWidget) {
+            $this->variable = $observer->parent()->getEval()->registerVar($this->makeVariableName());
+            $this->setValue($this->value);
+        }
+    }
+
+    protected function makeVariableName(): string
+    {
+        return 'radio-item-' . $this->id();
     }
 }
