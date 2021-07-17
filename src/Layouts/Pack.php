86311f501596cecbda1c5f106cd12e3c87438292
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-07-17 11:45:48 +0300

    layout pack refactoring

--- src/Layouts/Pack.php
+++ src/Layouts/Pack.php
@@ -10,12 +10,6 @@ use PhpGui\Widgets\Widget;
  *
  * @link https://www.tcl.tk/man/tcl8.6/TkCmd/pack.htm
  *
- * @property string $side
- * @property string $fill
- * @property bool $expand
- * @property string $ipadx
- * @property string $ipady
- * @property string $anchor
  */
 class Pack extends Manager
 {
@@ -28,7 +22,10 @@ class Pack extends Manager
     const FILL_Y = 'y';
     const FILL_BOTH = 'both';
 
-    protected function initOptions(): Options
+    /**
+     * @inheritdoc
+     */
+    protected function createLayoutOptions(): Options
     {
         return new Options([
             'side' => null,
@@ -44,121 +41,11 @@ class Pack extends Manager
         ]);
     }
 
-    public function side(string $side): self
-    {
-        $this->side = $side;
-        return $this;
-    }
-
-    public function sideLeft(): self
-    {
-        return $this->side(self::SIDE_LEFT);
-    }
-
-    public function sideRight(): self
-    {
-        return $this->side(self::SIDE_RIGHT);
-    }
-
-    public function sideTop(): self
-    {
-        return $this->side(self::SIDE_TOP);
-    }
-
-    public function sideBottom(): self
-    {
-        return $this->side(self::SIDE_BOTTOM);
-    }
-
-    public function fill(string $fill): self
-    {
-        $this->fill = $fill;
-        return $this;
-    }
-
-    public function fillX(): self
-    {
-        return $this->fill(self::FILL_X);
-    }
-
-    public function fillY(): self
-    {
-        return $this->fill(self::FILL_Y);
-    }
-
-    public function fillBoth(): self
-    {
-        return $this->fill(self::FILL_BOTH);
-    }
-
-    public function expand(): self
-    {
-        $this->expand = 1;
-        return $this;
-    }
-
-    /**
-     * @param int|string $amount
-     */
-    public function ipadX($amount): self
-    {
-        $this->ipadx = $amount;
-        return $this;
-    }
-
-    /**
-     * @param int|string $amount
-     */
-    public function ipadY($amount): self
-    {
-        $this->ipady = $amount;
-        return $this;
-    }
-
-    /**
-     * @link https://www.tcl.tk/man/tcl8.6/TkCmd/pack.htm#M19
-     */
-    public function padX($amount): self
-    {
-        $this->padx = $amount;
-        return $this;
-    }
-
-    /**
-     * @link https://www.tcl.tk/man/tcl8.6/TkCmd/pack.htm#M20
-     */
-    public function padY($amount): self
-    {
-        $this->pady = $amount;
-        return $this;
-    }
-
-    public function pad($amountX, $amountY): self
-    {
-        $this->padx = $amountX;
-        $this->pady = $amountY;
-        return $this;
-    }
-
-    public function ipad($amountX, $amountY): self
-    {
-        $this->ipadx = $amountX;
-        $this->ipady = $amountY;
-        return $this;
-    }
-
     /**
      * @inheritdoc
      */
-    public function manage(): Widget
+    protected function command(): string
     {
-        $this->call('pack');
-        return parent::manage();
-    }
-
-    public function anchor(string $dir): self
-    {
-        $this->anchor = $dir;
-        return $this;
+        return 'pack';
     }
 }
