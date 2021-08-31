78af6ed002806972599b8c294ab2c8587719c312
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-08-31 20:44:39 +0300

    Contract for clickable widget.

---
+++ src/Widgets/Common/Clickable.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace PhpGui\Widgets\Common;
+
+/**
+ * The widget that can handle 'click' event from the user.
+ */
+interface Clickable
+{
+    /**
+     * Handle a click event from the widget.
+     */
+    public function onClick(callable $callback): self;
+
+    /**
+     * Emulate a click event.
+     */
+    public function invoke(): self;
+}
