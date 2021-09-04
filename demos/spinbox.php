b765e4fa3f1cb70203d27b2a8e783776d94b3770
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-09-04 12:09:44 +0300

    spinbox widget and demo

---
+++ demos/spinbox.php
@@ -0,0 +1,46 @@
+<?php
+
+use PhpGui\Layouts\Pack;
+use PhpGui\Widgets\Spinbox;
+
+require_once dirname(__FILE__) . '/DemoAppWindow.php';
+
+$demo = new class extends DemoAppWindow
+{
+    public function __construct()
+    {
+        parent::__construct('Spinbox Demo');
+        $this->getWindowManager()->setSize(240, 200);
+        $this->buildUI();
+    }
+
+    private function buildUI()
+    {
+        $sp1 = new Spinbox($this, 1, [
+            'from' => 1,
+            'to' => 10,
+            'width' => 20,
+        ]);
+
+        $sp2 = new Spinbox($this, 0.00, [
+            'from' => 0,
+            'to' => 3,
+            'increment' => 0.5,
+            'format' => '%05.2f',
+            'width' => 20,
+        ]);
+
+        $sp3 = new Spinbox($this, 'Canberra', [
+            'values' => ['Canberra', 'Sydney', 'Melbourne', 'Perth', 'Adelaide', 'Brisbane', 'Hobart', 'Darwin', 'Alice Springs'],
+            'width' => 20,
+        ]);
+
+        $this->pack([$sp1, $sp2, $sp3], [
+            'side' => Pack::SIDE_TOP,
+            'pady' => 5,
+            'padx' => 10,
+        ]);
+    }
+};
+
+$demo->run();
---
+++ src/Widgets/Spinbox.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace PhpGui\Widgets;
+
+use PhpGui\Options;
+use PhpGui\TclTk\Variable;
+use PhpGui\Widgets\Common\ValueInVariable;
+
+/**
+ * Implementation of Ttk spinbox widget.
+ *
+ * @link https://www.tcl.tk/man/tcl8.6/TkCmd/ttk_spinbox.html
+ *
+ * @property callable $command
+ * @property string $format
+ * @property float $from
+ * @property float $increment
+ * @property float $to
+ * @property array $values
+ * @property bool $wrap
+ * @property Variable $textVariable
+ */
+class Spinbox extends TtkWidget implements ValueInVariable
+{
+    protected string $widget = 'ttk::spinbox';
+    protected string $name = 'spnb';
+
+    /**
+     * @param string|int|float $value
+     */
+    public function __construct(Container $parent, $value, array $options = [])
+    {
+        parent::__construct($parent, $options);
+        $this->setValue($value);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function initWidgetOptions(): Options
+    {
+        return new Options([
+            'command' => null,
+            'format' => null,
+            'from' => null,
+            'increment' => null,
+            'to' => null,
+            'values' => null,
+            'wrap' => null,
+            'textVariable' => null,
+        ]);
+    }
+
+    public function onIncrement(callable $callback): self
+    {
+        $this->bind('<<Increment>>', $callback);
+        return $this;
+    }
+
+    public function onDecrement(callable $callback): self
+    {
+        $this->bind('<<Decrement>>', $callback);
+        return $this;
+    }
+
+    public function getValue()
+    {
+        return $this->call('get');
+    }
+
+    public function setValue($value): self
+    {
+        $this->call('set', $value);
+        return $this;
+    }
+}
