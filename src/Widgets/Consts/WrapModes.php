90e3800186a1850c41f8b51bf98f22a859eb1c8f
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-09-11 11:12:56 +0300

    values for "wrap" property.

---
+++ src/Widgets/Consts/WrapModes.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace PhpGui\Widgets\Consts;
+
+/**
+ * Values for "wrap" property.
+ */
+interface WrapModes
+{
+    const WRAP_NONE = 'none';
+    const WRAP_CHAR = 'char';
+    const WRAP_WORD = 'word';
+}
