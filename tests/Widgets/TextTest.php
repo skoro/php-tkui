23e15d11ba767c34d476ed637ed38b3434bbf6f2
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-09-16 21:36:54 +0300

    additional text test cases

--- tests/Widgets/TextTest.php
+++ tests/Widgets/TextTest.php
@@ -4,6 +4,7 @@ namespace PhpGui\Tests\Widgets;
 
 use PhpGui\Tests\TestCase;
 use PhpGui\Widgets\Text\Text;
+use PhpGui\Widgets\Text\TextIndex;
 
 class TextTest extends TestCase
 {
@@ -31,6 +32,18 @@ class TextTest extends TestCase
     }
 
     /** @test */
+    public function append_text_with_style()
+    {
+        $this->tclEvalTest(3, [
+            ['text', $this->checkWidget('.t')],
+            [$this->checkWidget('.t'), 'tag', 'configure', 'sel'],
+            [$this->checkWidget('.t'), 'insert', 'end', 'Text text text', ['myStyle']],
+        ]);
+
+        (new Text($this->createWindowStub()))->appendWithStyle('Text text text', 'myStyle');
+    }
+
+    /** @test */
     public function clear_all_contents()
     {
         $this->tclEvalTest(3, [
@@ -55,5 +68,72 @@ class TextTest extends TestCase
         (new Text($this->createWindowStub()))
             ->append('test')
             ->getContent();
+    }
+
+    /** @test */
+    public function delete_text_range()
+    {
+        $this->tclEvalTest(3, [
+            ['text', $this->checkWidget('.t')],
+            [$this->checkWidget('.t'), 'tag', 'configure', 'sel'],
+            [$this->checkWidget('.t'), 'delete', '3.12', '101.0'],
+        ]);
+
+        (new Text($this->createWindowStub()))
+            ->delete(new TextIndex(3, 12), new TextIndex(101, 0))
+            ;
+    }
+
+    /** @test */
+    public function insert_text()
+    {
+        $this->tclEvalTest(3, [
+            ['text', $this->checkWidget('.t')],
+            [$this->checkWidget('.t'), 'tag', 'configure', 'sel'],
+            [$this->checkWidget('.t'), 'insert', '5.4', 'new text'],
+        ]);
+
+        (new Text($this->createWindowStub()))->insert(new TextIndex(5, 4), 'new text');
+    }
+
+    /** @test */
+    public function insert_with_style()
+    {
+        $this->tclEvalTest(3, [
+            ['text', $this->checkWidget('.t')],
+            [$this->checkWidget('.t'), 'tag', 'configure', 'sel'],
+            [$this->checkWidget('.t'), 'insert', '15.40', 'new text', ['myStyle1', 'myStyle2']],
+        ]);
+
+        (new Text($this->createWindowStub()))->insert(new TextIndex(15, 40), 'new text', 'myStyle1', 'myStyle2');
+    }
+
+    /** @test */
+    public function get_cursor_pos()
+    {
+        $this->tclEvalTest(3, [
+            ['text', $this->checkWidget('.t')],
+            [$this->checkWidget('.t'), 'tag', 'configure', 'sel'],
+            [$this->checkWidget('.t'), 'index', 'insert'],
+        ]);
+
+        try {
+            (new Text($this->createWindowStub()))->getCursorPos();
+        } catch (\Exception $e) {
+            // Ignore TextIndex exception
+            // We must check only tcl command.
+        }
+    }
+
+    /** @test */
+    public function set_cursor_pos()
+    {
+        $this->tclEvalTest(3, [
+            ['text', $this->checkWidget('.t')],
+            [$this->checkWidget('.t'), 'tag', 'configure', 'sel'],
+            [$this->checkWidget('.t'), 'mark', 'set', 'insert', '11.25'],
+        ]);
+
+        (new Text($this->createWindowStub()))->setCursorPos(new TextIndex(11, 25));
     }
 }
