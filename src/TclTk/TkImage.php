0b97a434cef74e9d1e71bb2ded4259c1d920318e
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-11-30 22:19:04 +0200

    Tk implementation of image

---
+++ src/TclTk/TkImage.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace PhpGui\TclTk;
+
+use PhpGui\Image;
+
+/**
+ * Tk implementation of Image.
+ */
+class TkImage implements Image
+{
+    private Interp $interp;
+    private string $id;
+
+    public function __construct(Interp $interp, string $id)
+    {
+        $this->interp = $interp;
+        $this->id = $id;
+    }
+
+    public function width(): int
+    {
+        $this->interp->eval("image width {$this->id}");
+        return (int) $this->interp->getStringResult();
+    }
+
+    public function height(): int
+    {
+        $this->interp->eval("image height {$this->id}");
+        return (int) $this->interp->getStringResult();
+    }
+
+    public function __toString(): string
+    {
+        return $this->id;
+    }
+}
---
+++ src/TclTk/TkImageFactory.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace PhpGui\TclTk;
+
+use PhpGui\Image;
+use PhpGui\ImageFactory;
+
+/**
+ * Tk implementation of Image Factory.
+ */
+class TkImageFactory implements ImageFactory
+{
+    private Interp $interp;
+
+    public function __construct(Interp $interp)
+    {
+        $this->interp = $interp;
+    }
+
+    public function createFromFile(string $filename): Image
+    {
+        $this->interp->eval(sprintf('image create photo -file {%s}', $filename));
+        return $this->createImage();
+    }
+
+    public function createFromBinary(string $data): Image
+    {
+        $this->interp->eval(sprintf('image create photo -data {%s}', $data));
+        return $this->createImage();
+    }
+
+    protected function createImage(): Image
+    {
+        $id = $this->interp->getStringResult();
+
+        return new TkImage($this->interp, $id);
+    }
+}
