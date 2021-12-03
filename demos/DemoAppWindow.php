ce0f776671238d08c0bea25c8a5b522f043c64e2
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2021-12-03 21:30:39 +0200

    helper for loading images

--- demos/DemoAppWindow.php
+++ demos/DemoAppWindow.php
@@ -2,6 +2,7 @@
 
 use PhpGui\Application;
 use PhpGui\DotEnv;
+use PhpGui\Image;
 use PhpGui\TclTk\TkAppFactory;
 use PhpGui\Windows\MainWindow;
 
@@ -10,16 +11,23 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 class DemoAppWindow extends MainWindow
 {
     protected Application $app;
+    private string $imageDir;
 
     public function __construct(string $title)
     {
         $factory = new TkAppFactory();
         $this->app = $factory->createFromEnvironment(DotEnv::create(dirname(__DIR__)));
         parent::__construct($this->app, $title);
+        $this->imageDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'icons' . DIRECTORY_SEPARATOR;
     }
 
     public function run(): void
     {
         $this->app->run();
+    }
+
+    protected function loadImage(string $filename): Image
+    {
+        return $this->app->getImageFactory()->createFromFile($this->imageDir . $filename);
     }
 }
